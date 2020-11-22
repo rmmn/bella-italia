119d1d8b1684e7ec72eb5804ca7e1899a7f026b5
rmmn <43585084+rmmn@users.noreply.github.com>
2020-11-22 18:24:51 +0300

    adaptive and js

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,7 +14,10 @@
         variantListener = document.querySelector('.variants-listener'),
         selectVariantButtons = document.querySelectorAll('.variant-buttons>.variant-button'),
         buttonVariantFeedback = document.querySelector('.variant-buttons>.variant-button[data-plan="feedback"]'),
-        buttonVariantListener = document.querySelector('.variant-buttons>.variant-button[data-plan="listener"]');
+        buttonVariantListener = document.querySelector('.variant-buttons>.variant-button[data-plan="listener"]'),
+        menuItems = document.querySelectorAll('.top-menu>li>.nav-link'),
+        navOverlay = document.querySelector('.nav-overlay'),
+        menu = document.querySelector('.menu');
 
     window.addEventListener('scroll', function (e) {
         if (this.scrollY < (header.clientHeight - nav.clientHeight)) {
@@ -22,6 +25,16 @@
         } else {
             navBg.classList.add('shown');
         }
+
+        // menuItems.forEach(m => {
+        //     const id = m.getAttribute('href').split('#')[1];
+
+        //     if (isElementInViewport(document.getElementById(id))) {
+        //         m.classList.add('active');
+        //     } else {
+        //         m.classList.remove('active');
+        //     }
+        // })
     });
 
     icons.forEach(icon => {
@@ -93,4 +106,99 @@
 
         });
     });
+
+    menuItems.forEach(menuItem => {
+        menuItem.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const id = this.getAttribute('href').split('#')[1],
+                yOffset = -nav.clientHeight,
+                element = document.getElementById(id),
+                y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+
+            window.scrollTo({ top: y, behavior: 'smooth' });
+
+            window.history.pushState(null, "", window.location.href.split('#')[0] + '#' + id);
+
+            menuItems.forEach(m => {
+                if (m.getAttribute('href') == '#' + id) {
+                    m.classList.add('active');
+                } else {
+                    m.classList.remove('active');
+                }
+            });
+
+            navOverlay.classList.remove('opened');
+            menu.classList.remove('open');
+
+            if (window.scrollY > (header.clientHeight - nav.clientHeight)) {
+                navBg.classList.remove('shown');
+            }
+
+            if (window.innerWidth <= 768 || window.innerHeight <= window.innerHeight < (document.querySelector('.top-menu').clientHeight + 174)) {
+                document.body.removeAttribute('style');
+                document.querySelector("html").removeAttribute('style');
+            }
+        });
+    });
+
+    menu.addEventListener('click', function () {
+        if (this.classList.contains('open')) {
+            this.classList.remove('open');
+            navOverlay.classList.remove('opened');
+            navBg.classList.remove('shown');
+
+            if (window.scrollY > (header.clientHeight - nav.clientHeight)) {
+                navBg.classList.add('shown');
+            }
+
+            if (window.innerWidth <= 768 || window.innerHeight < (document.querySelector('.top-menu').clientHeight + 174)) {
+                document.body.removeAttribute('style');
+                document.querySelector("html").removeAttribute('style');
+            }
+        } else {
+            this.classList.add('open');
+            navOverlay.classList.add('opened');
+            navBg.classList.add('shown');
+
+            if (window.innerWidth <= 768 || window.innerHeight < (document.querySelector('.top-menu').clientHeight + 174)) {
+                document.body.style.overflow = "hidden";
+                document.querySelector("html").style.overflow = "hidden";
+            }
+        }
+    });
+
+    scrollWhenLoaded();
+
+    function scrollWhenLoaded() {
+        const hash = window.location.hash;
+
+        if (hash != '') {
+            const id = hash.split('#')[1],
+                yOffset = -nav.clientHeight,
+                element = document.getElementById(id),
+                y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+
+            window.scrollTo({ top: y, behavior: 'smooth' });
+
+            menuItems.forEach(menuItem => {
+                if (menuItem.getAttribute('href') == hash) {
+                    menuItem.classList.add('active');
+                } else {
+                    menuItem.classList.remove('active');
+                }
+            });
+        }
+    }
+
+    function isElementInViewport(el) {
+        var rect = el.getBoundingClientRect();
+
+        return rect.bottom + 60 >= 0 &&
+            rect.right > 0 &&
+            rect.left < (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */ &&
+            rect.top + 60 < (window.innerHeight || document.documentElement.clientHeight) /* or $(window).height() */;
+    }
+
+
 })();
